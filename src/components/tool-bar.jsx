83e2d463603e9cffbe1466fe49db5c1641b878e7
Prakhar543 <prakhar.agra5@gmail.com>
2025-04-08 19:38:46 +0530

    tool bar ui updated

--- src/components/tool-bar.jsx
+++ src/components/tool-bar.jsx
@@ -1,42 +1,149 @@
-"use client"
-import React from "react"
-import { MousePointer, Square, RectangleHorizontal, Undo, Redo } from "lucide-react"
-import { Button } from "../components/ui/button"
+"use client";
+import React from "react";
+import {
+  MousePointer,
+  Square,
+  BrickWall,
+  Undo,
+  Redo,
+  Trash2,
+} from "lucide-react";
+import { Button } from "../components/ui/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../components/ui/tooltip";
 
-export default function ToolBar({ selectedTool, setSelectedTool, clearCanvas, saveShapes }) {
+export default function ToolBar({
+  selectedTool,
+  setSelectedTool,
+  clearCanvas,
+  saveShapes,
+}) {
   const tools = [
-    { name: "pointer", icon: <MousePointer size={18} color="#333" />, bgColor: "bg-[#F3F4F6]" },
-    { name: "rectangle", icon: <Square size={18} color="#333" />, bgColor: "bg-[#F3F4F6]" },
+    { name: "pointer", icon: <MousePointer size={18} color="#333" /> },
+    { name: "rectangle", icon: <Square size={18} color="#333" /> },
+    {
+      name: "walls",
+      icon: (
+        <img
+          src="/walls.svg"
+          style={{
+            filter:
+              selectedTool === "walls"
+                ? "invert(100%) sepia(100%) saturate(0%) hue-rotate(180deg)"
+                : "none",
+          }}
+          size={18}
+          color="#333"
+        />
+      ),
+    },
+    {
+      name: "delete",
+      icon: (
+        <img
+          src="/bin.svg"
+          style={{
+            filter:
+              selectedTool === "delete"
+                ? "invert(100%) sepia(100%) saturate(0%) hue-rotate(180deg)"
+                : "none",
+          }}
+          size={18}
+          color="#333"
+        />
+      ),
+    },
+    {
+      name: "fill",
+      icon: (
+        <img
+          src="/paint-bucket.svg"
+          style={{
+            filter:
+              selectedTool === "fill"
+                ? "invert(100%) sepia(100%) saturate(0%) hue-rotate(180deg)"
+                : "none",
+          }}
+          size={18}
+          color="#333"
+        />
+      ),
+    },
+    {
+      name: "open-space",
+      icon: (
+        <img
+          src="/open-space.svg"
+          style={{
+            filter:
+              selectedTool === "open-space"
+                ? "invert(100%) sepia(100%) saturate(0%) hue-rotate(180deg)"
+                : "none",
+          }}
+          size={18}
+          color="#333"
+        />
+      ),
+    },
     // { name: "rectangle-h", icon: <RectangleHorizontal size={18} color="#333" />, bgColor: "bg-[#F3F4F6]" },
-  ]
+  ];
 
   return (
     <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-white rounded-full shadow-lg p-2 pl-4 pr-4 flex items-center gap-2">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 mr-6">
         {tools.map((tool) => (
-          <Button
-            key={tool.name}
-            variant="ghost"
-            size="icon"
-            onClick={() => setSelectedTool(tool.name)}
-            className={`${tool.bgColor} rounded-md h-9 w-9 ${selectedTool === tool.name ? " bg-[#6366F1] ring-2 ring-blue-300" : ""}`}
-          >
-            {React.cloneElement(tool.icon, { color: selectedTool===tool.name ? "#FFF" : "#333" })}
-          </Button>
+          <div className="relative group">
+            <Button
+              key={tool.name}
+              variant="ghost"
+              size="icon"
+              onClick={() => setSelectedTool(tool.name)}
+              className={`rounded-md h-9 w-9 ${
+                selectedTool === tool.name
+                  ? " bg-[#6366F1] ring-2 ring-blue-300"
+                  : "hover:bg-[#F3F4F6]"
+              } `}
+            >
+              {React.cloneElement(tool.icon, {
+                color: selectedTool === tool.name ? "#FFF" : "#333",
+              })}
+            </Button>
+            <div className="absolute w-[100px] bottom-full left-1/2 transform -translate-x-1/2 mb-2 hidden group-hover:flex flex-col items-center z-10">
+              {/* Arrow */}
+              <div className="w-2 h-2 bg-gray-800 rounded-[2px] rotate-45 translate-y-[27px]"></div>
+              {/* Tooltip box */}
+              <div className="bg-gray-800 text-white text-xs rounded px-2 py-1 shadow-md">
+                {tool.name}
+              </div>
+            </div>
+          </div>
         ))}
+        <Button className={`rounded-md bg-[#F0F4FF] text-[#717AEA]`}>
+          <span className="flex justify-between">
+            <img src="/upload.svg" className="mr-2" /> Upload
+          </span>
+        </Button>
       </div>
 
-      <div className="flex items-center gap-2 ml-1">
+      {/* <div className="flex items-center gap-2 ml-1">
         <Button variant="ghost" size="icon" className="bg-[#F3F4F6] rounded-md h-9 w-9">
           <Undo size={18} color="#333" />
         </Button>
         <Button variant="ghost" size="icon" className="bg-[#F3F4F6] rounded-md h-9 w-9">
           <Redo size={18} color="#333" />
         </Button>
-      </div>
+      </div> */}
 
       <div className="flex items-center gap-2 ml-2">
-        <Button variant="outline" onClick={clearCanvas} className="rounded-md h-9 px-4 text-sm font-medium text-black">
+        <Button
+          variant="outline"
+          onClick={clearCanvas}
+          className="rounded-md h-9 px-4 text-sm font-medium text-black"
+        >
           Discard
         </Button>
         <Button
@@ -48,6 +155,5 @@ export default function ToolBar({ selectedTool, setSelectedTool, clearCanvas, sa
         </Button>
       </div>
     </div>
-  )
+  );
 }
-
